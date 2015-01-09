0802a8ad0c447730928ebb095c20e42577d14bc3
psromero6 <promero6@unm.edu>
2015-01-09 11:36:04 -0700

    <Message>

---
+++ TerrainGenerator1/Assets/HeightMapGen.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeightMapGen : MonoBehaviour {
+	public Terrain terrain;
+	private float[,] h=new float[100,100];
+	private float[,] heights;
+	// Use this for initialization
+	void Start () {
+		int y, x,xRes,yRes;
+		xRes = terrain.terrainData.heightmapWidth;
+		yRes = terrain.terrainData.heightmapHeight;
+		heights=new float[xRes,yRes];
+		for (y = 0; y<yRes; y++) {
+			for (x = 0; x <xRes; x++) {
+				heights[x,y] = Random.Range(0.0f, 0.02f) * .5f; 
+			}
+		}
+
+		terrain.terrainData.SetHeights(0, 0, heights);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
